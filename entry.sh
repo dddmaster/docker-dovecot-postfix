b4e6a9c78f3b9508545cb304d3786a10575a0019
dddmaster <github@monster-z.de>
2023-03-29 00:21:44 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 if [[ "$1" == "dovecot" ]]; then
+	mkdir -p /var/spool/postfix/etc/ssl/certs/
+	cp /etc/resolv.conf /var/spool/postfix/etc/
+	cp /etc/ssl/certs/ca-certificates.crt /var/spool/postfix/etc/ssl/certs/
 	postfix start
 	/usr/bin/tini -- /usr/sbin/dovecot -F
 else 
