15e25dda8f0c11b9f4f12efa3e011570a3b52d34
DDDMaster <dddmaster@dddmaster.de>
2023-03-31 15:49:37 +0200

    postfix logging

--- entry.sh
+++ entry.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 if [[ "$1" == "dovecot" ]]; then
-	mkdir -p /var/spool/postfix/etc/ssl/certs/
-	cp /etc/resolv.conf /var/spool/postfix/etc/
-	cp /etc/ssl/certs/ca-certificates.crt /var/spool/postfix/etc/ssl/certs/
-	postfix start
-	/usr/bin/tini -- /usr/sbin/dovecot -F
+	mkdir -p /var/spool/postfix/etc/ssl/
+	#cp /etc/resolv.conf /var/spool/postfix/etc/
+	ln -s /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+	#cp /etc/ssl/certs/ca-certificates.crt /var/spool/postfix/etc/ssl/certs/
+	ln -s /etc/ssl/certs/ /var/spool/postfix/etc/ssl/
+	postfix start-fg &
+	#/usr/bin/tini -- /usr/sbin/dovecot -F
+	/usr/sbin/dovecot -F
 else 
 	exec $@
 fi
