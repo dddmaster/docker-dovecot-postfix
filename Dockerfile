cc1bcd02f10aec34170083e2e28a1c056158965a
DDDMaster <dddmaster@dddmaster.de>
2023-04-18 23:43:03 +0200

    healthcheck and error fix

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM dovecot/dovecot
 ENV DEBIAN_FRONTEND noninteractive
 COPY entry.sh /entry.sh
 RUN apt-get -y update \
-	&& apt-get install postfix postfix-mysql -y \
+	&& apt-get install postfix postfix-mysql procps -y \
 	&& chmod u+x entry.sh \
 	&& apt-get clean \
 	&& apt-get autoremove --yes \
 	&& rm -rf /var/lib/{apt,cache,log}/ \
 	&& usermod -a -G dovecot postfix
+HEALTHCHECK --interval=120s --timeout=30s \
+	CMD (ps -aux | grep 'postfix' | grep -v grep) && (ps -aux | grep 'dovecot -F' | grep -v grep) || exit 1
 ENTRYPOINT ["/entry.sh"]
 CMD ["dovecot"]
--- entry.sh
+++ entry.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 if [[ "$1" == "dovecot" ]]; then
 	mkdir -p /var/spool/postfix/etc/ssl/
-	#cp /etc/resolv.conf /var/spool/postfix/etc/
-	ln -s /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
-	#cp /etc/ssl/certs/ca-certificates.crt /var/spool/postfix/etc/ssl/certs/
-	ln -s /etc/ssl/certs/ /var/spool/postfix/etc/ssl/
+	
+	if [[ ! -e /var/spool/postfix/etc/resolv.conf ]]; then
+		ln -s /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+	fi
+
+	if [[ ! -d /var/spool/postfix/etc/ssl/certs ]]; then
+		ln -s /etc/ssl/certs/ /var/spool/postfix/etc/ssl/
+	fi
+	
 	postfix start-fg &
 	#/usr/bin/tini -- /usr/sbin/dovecot -F
 	/usr/sbin/dovecot -F
